088f6daa232ee426796f78b522136bba5b9528a5
victor DG <v.degermain@gmail.com>
2022-06-14 15:16:11 +0200

    first commit and challenge good

--- index.js
+++ index.js
@@ -1,35 +1,53 @@
-console.log('Test fetch');
+const pokemonId = 1;
+const data = {
+    quote:
+        "These are my only friends...grown-up nerds like Gore Vidal. And even he's kissed more boys than I ever will.",
+    character: 'Lisa Simpson',
+    image:
+        'https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FLisaSimpson.png?1497567512083',
+    characterDirection: 'Right'
+}
 
-function fetchAdditionalHTML() {
-    fetch('https://wildcodeschool.github.io/ajax-quest/html/additional-content.html')
-        .then(res => {
-            console.log(res.text)
-            return res.text();
+const pokemonHtml = `<p> ${data.quote} </p>`;
+const card = document.querySelector('#simpson');
+const cardImg = card.querySelector("#card-img");
+const cardName = card.querySelector("#card-name");
+const cardCitation = card.querySelector("#card-citation");
+const button = card.querySelector("button");
+
+const url = `https://simpsons-quotes-api.herokuapp.com/quotes`;
+
+
+button.addEventListener("click", (e) => {
+    // console.log('refresh card');
+     e.preventDefault;
+    fetchPokemonJSON();
+})
+
+// This function loads pokemon data from the Pokemon API
+function fetchPokemonJSON() {
+    // Feel free to download this HTML and edit it, to use another Pokemon ID
+
+
+    
+    axios.get(url)
+        .then( (response) =>{
+            return response.data; // response.data instead of response.json() with fetch
         })
-        .then(htmlContent => {
-            console.log('received data:', htmlContent);
+        .then( (pokemon) =>{
+            console.log('data decoded from JSON:', pokemon);
+
+       
            
-            document.querySelector('#additional-content').innerHTML = htmlContent;
+           
+            
+            cardName.querySelector("p").innerHTML = pokemon[0].character;
+             cardImg.src=pokemon[0].image;
+             console.log(cardCitation)
+            cardName.querySelector("blockquote").innerHTML =pokemon[0].quote;
         });
 }
 
-const fetchGitHubProfileJSON =() =>{
-    const username = 'letotor';
-    const url = `https://api.github.com/users/${username}`;
-    fetch(url)
-        .then( (res) =>{
-            return res.json();
-        })
-        .then( (profile) =>{
-            const profileHtml = `
-        <p><strong>${profile.name}</strong></p>
-        <img src="${profile.avatar_url}" />
-      `;
-            document.querySelector('#github-profile').innerHTML = profileHtml;
-        });
-}
-
-fetchGitHubProfileJSON();
 
 
-setTimeout(fetchAdditionalHTML, 3000);
+fetchPokemonJSON();
